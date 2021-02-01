94839a3d9cf45cd31b58968bac67bb3149c7e592
arjun.panwar <arjun.panwar@tothenew.com>
2021-02-01 19:37:39 +0530

    stackexchange: stackExchange API endpoint created

--- dev/searchQuery.js
+++ dev/searchQuery.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const searchQueryHelper = require('./searchQueryHelper');
 const bodyParser = require('body-parser');
 const requestPromise = require('request-promise');
+const https = require('https');
+const zlib = require("zlib");
 const {v4: uuidv4} = require('uuid');
 // Watson languageTranslator..........
 const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 // Stackexchange Api..................
-const stackexchange = require('stackexchange');
-const options = { version: 2.2 };
-const context = new stackexchange(options);
+// const stackexchange = require('stackexchange');
+// const options = { version: 2.2 };
+// const context = new stackexchange(options);
 
 var app = express();
+const searchQuery = new searchQueryHelper();
 
 // Watson Language Translator API Configuration.....
 const languageTranslator = new LanguageTranslatorV3({
@@ -22,31 +26,33 @@ const languageTranslator = new LanguageTranslatorV3({
 });
 
 // Get input Query value.........
-app.get('/home/:queryString', function(req, res){
-    const query = req.params.queryString;
+app.get('/home/:queryString/:questionCount', function(req, res){
+    var queryString = req.params.queryString;
+    const questionCount = req.params.questionCount;
     const identifyParams = {
-        text: query
+        text: queryString
     };
     languageTranslator.identify(identifyParams)
     .then(identifiedLanguages => {
         var language = identifiedLanguages.result.languages[0].language;
         if(language !== "en") {
             const translateParams = {
-                text: query,
+                text: queryString,
                 modelId: `${language}-en`,
             };
             languageTranslator.translate(translateParams)
             .then(translationResult => {
                 var queryString = translationResult.result.translations[0].translation;
                 res.json(queryString);
+                searchQuery.stackExchange(queryString, questionCount);
             })
             .catch(err => {
                 res.json('error:', err);
             });
         }
         else {
-            var queryString = query;
             res.json(queryString);
+            searchQuery.stackExchange(queryString, questionCount);
         }
     })
     .catch(err => {
@@ -55,7 +61,7 @@ app.get('/home/:queryString', function(req, res){
 })
 
 // Define port...................
-app.listen(3004, function(){
+app.listen(3000, function(){
     console.log("App is started on 3000.");
 })
 
--- dev/searchQueryHelper.js
+++ dev/searchQueryHelper.js
@@ -1,1 +1,32 @@
-console.log("Sbkuch m krk dedu tumlog kya kroge 🤣🤣");
+var https = require('https');
+var zlib = require("zlib");
+
+class searchQueryHelper {
+    stackExchange(queryString, questionCount) {
+        var Auth_key = "36MG*PWxNRuR2IurlPc8vg((";
+        var page_size = parseInt(questionCount) + 10;
+        var query = queryString.toString();
+        var stackOverflowUserURL = "https://api.stackexchange.com/2.2/similar?key=" + Auth_key + "&order=desc&page=1&pagesize=" + page_size + "&sort=relevance&title=" + query + "&site=stackoverflow";
+        https.get(stackOverflowUserURL, function(response){
+            if (response.statusCode == 200) {
+                var gunzip = zlib.createGunzip();
+                var jsonString = '';
+                response.pipe(gunzip);
+                    gunzip.on('data', function (chunk) {
+                    jsonString += chunk;
+                });
+                gunzip.on('end', function () {
+                    console.log(JSON.parse(jsonString));
+                });
+                gunzip.on('error', function (e) {
+                    console.log(e);
+                });
+            }
+            else{
+                console.log("Error");
+            }
+        });
+    }
+}
+
+module.exports = searchQueryHelper;
