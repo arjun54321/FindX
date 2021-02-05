967f0e867a349ccf9264472c4a1441831cec6007
arjun.panwar <arjun.panwar@tothenew.com>
2021-02-05 21:03:37 +0530

    stackexchange: sentimental analysis for answers

--- dev/searchQuery.js
+++ dev/searchQuery.js
@@ -1,77 +1,81 @@
-const express = require('express');
-const searchQueryHelper = require('./searchQueryHelper');
-const bodyParser = require('body-parser');
-const requestPromise = require('request-promise');
-const https = require('https');
+const express = require("express");
+const searchQueryHelper = require("./searchQueryHelper");
+const bodyParser = require("body-parser");
+const requestPromise = require("request-promise");
+const https = require("https");
 const zlib = require("zlib");
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 // Watson languageTranslator..........
-const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
-// Stackexchange Api..................
-// const stackexchange = require('stackexchange');
-// const options = { version: 2.2 };
-// const context = new stackexchange(options);
+const LanguageTranslatorV3 = require("ibm-watson/language-translator/v3");
+const { IamAuthenticator } = require("ibm-watson/auth");
 
 var app = express();
 const searchQuery = new searchQueryHelper();
 
 // Watson Language Translator API Configuration.....
 const languageTranslator = new LanguageTranslatorV3({
-  version: '2018-05-01',
+  version: "2018-05-01",
   authenticator: new IamAuthenticator({
-    apikey: 'owATBr1PcPdXG9KsV0jEe_-xxtvrypJ7gAYHl7uyxVcm',
+    apikey: "owATBr1PcPdXG9KsV0jEe_-xxtvrypJ7gAYHl7uyxVcm",
   }),
-  serviceUrl: 'https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/487d1371-8841-4279-a79e-73336dd67bea',
+  serviceUrl:
+    "https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/487d1371-8841-4279-a79e-73336dd67bea",
 });
 
 // Get input Query value.........
-app.get('/home/:queryString/:questionCount', function(req, res){
-    var queryString = req.params.queryString;
-    const questionCount = req.params.questionCount;
-    const identifyParams = {
-        text: queryString
-    };
-    languageTranslator.identify(identifyParams)
-    .then(identifiedLanguages => {
-        var language = identifiedLanguages.result.languages[0].language;
-        if(language !== "en") {
-            const translateParams = {
-                text: queryString,
-                modelId: `${language}-en`,
-            };
-            languageTranslator.translate(translateParams)
-            .then(translationResult => {
-                var queryString = translationResult.result.translations[0].translation;
-                searchQuery.stackExchange(queryString, questionCount, function(result) {
-                    res.status(200).json({
-                        status: 'succes',
-                        data: result,
-                    })
-                });
-            })
-            .catch(err => {
-                res.sendStatus('error:', err)
-            });
-        }
-        else {
-            searchQuery.stackExchange(queryString, questionCount, function(result) {
+app.get("/home/:queryString/:questionCount", function (req, res) {
+  var queryString = req.params.queryString;
+  const questionCount = req.params.questionCount;
+  const identifyParams = {
+    text: queryString,
+  };
+  languageTranslator
+    .identify(identifyParams)
+    .then((identifiedLanguages) => {
+      var language = identifiedLanguages.result.languages[0].language;
+      if (language !== "en") {
+        const translateParams = {
+          text: queryString,
+          modelId: `${language}-en`,
+        };
+        languageTranslator
+          .translate(translateParams)
+          .then((translationResult) => {
+            var queryString =
+              translationResult.result.translations[0].translation;
+            searchQuery.stackExchange(
+              queryString,
+              questionCount,
+              function (result) {
                 res.status(200).json({
-                    status: 'succes',
-                    data: result,
-                })
+                  status: "succes",
+                  data: result,
+                });
+              }
+            );
+          })
+          .catch((err) => {
+            res.sendStatus("error:", err);
+          });
+      } else {
+        searchQuery.stackExchange(
+          queryString,
+          questionCount,
+          function (result) {
+            res.status(200).json({
+              status: "succes",
+              data: result,
             });
-        }
+          }
+        );
+      }
     })
-    .catch(err => {
-        res.sendStatus('error:', err)
+    .catch((err) => {
+      res.sendStatus("error:", err);
     });
-})
+});
 
 // Define port...................
-app.listen(3000, function(){
-    console.log("App is started on 3000.");
-})
-
-
-
+app.listen(3000, function () {
+  console.log("App is started on 3000.");
+});
--- dev/searchQueryHelper.js
+++ dev/searchQueryHelper.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+var Sentiment = require("sentiment");
+var sentiment = new Sentiment();
 const Auth_key = "36MG*PWxNRuR2IurlPc8vg((";
 class searchQueryHelper {
   stackExchange(queryString, questionCount, callback) {
@@ -18,7 +20,7 @@ class searchQueryHelper {
       .then((response) => {
         if (response.status == 200) {
           self.getStackExchangeData(response.data, function (result) {
-            callback(result);
+            callback && callback(result);
           });
         } else {
           console.log("Error Occured");
@@ -36,7 +38,6 @@ class searchQueryHelper {
     var questionTitle = [];
     var actualTags = [];
     var answers_per_question = [];
-    var currentLink = 0;
     questionData["items"].forEach(function (item, index) {
       if (!item["is_answered"] || item["score"] < 0) {
         // pass
@@ -57,11 +58,7 @@ class searchQueryHelper {
     var answer_accepted = [];
     var score_answer = [];
     var auth_repo = [];
-    var test = [];
-    var len = Math.max(...answers_per_question);
-    var itemsProcessed = 0;
-    questionID.forEach(function (item, index) {
-      itemsProcessed++;
+    questionID.forEach(async function (item, index) {
       self.getStackExchangeQuestionById(item, function (result) {
         for (var i = 0; i < answers_per_question[index]; i++) {
           if (typeof result.items[i] !== "undefined") {
@@ -78,17 +75,83 @@ class searchQueryHelper {
             console.log("Not Defined");
           }
         }
-        console.log(answer_id);
       });
-      console.log("Helooooooooo.........");
     });
-    // Promise.all([comments_count]).then((values) => {
-    //   console.log(values);
-    // });
-  }
-
-  testing(x) {
-    console.log(x);
+    setTimeout(function () {
+      // if (comments_count.length === 0) {
+      //   callback && callback("No relevant data found");
+      // }
+      var k = 0;
+      var comments_sentiment = [];
+      //Sentimental Analysis for comments........
+      for (var i = 0; i < comments_count.length; i++) {
+        if (comments_count[i] === 0) {
+          comments_sentiment.push("neutral");
+        } else {
+          for (var j = 0; j < comments_count[i]; j++) {
+            var comment_string = comments_body[k];
+            k += 1;
+            var analysis = sentiment.analyze(comment_string);
+            if (analysis.comparative > 0) {
+              comments_sentiment.push("positive");
+            } else if (analysis.comparative == 0) {
+              comments_sentiment.push("neutral");
+            } else {
+              comments_sentiment.push("negative");
+            }
+          }
+        }
+      }
+      //Sentimental Analysis for answers.............
+      var DATA = [];
+      var answer_sentiment = [];
+      var k = 0;
+      for (var i = 0; i < questionLink.length; i++) {
+        var data = {};
+        data["link"] = questionLink[i];
+        data["id"] = questionID[i];
+        data["title"] = questionTitle[i];
+        data["actualTags"] = actualTags[i];
+        var ANSWERS = [];
+        var positive = 0;
+        var negative = 0;
+        for (var j = 0; j < answers_per_question[i]; j++) {
+          var answer = {};
+          answer["is_accepted"] = answer_accepted[j];
+          answer["answer_id"] = answer_id[j];
+          answer["score_answer"] = score_answer[j];
+          answer["authRepo"] = auth_repo[j];
+          answer["sentiment"] = comments_sentiment[k];
+          if (comments_sentiment[k] == "positive") {
+            positive += 1;
+          }
+          if (comments_sentiment[k] == "negative") {
+            negative += 1;
+          }
+          k += 1;
+          ANSWERS.push(answer);
+        }
+        data["answer"] = ANSWERS;
+        if (positive > negative) {
+          data["onclick_comments_sentiment"] = "positive";
+          answer_sentiment.push(1);
+        }
+        if (negative > positive) {
+          data["onclick_comments_sentiment"] = "negative";
+          answer_sentiment.push(0);
+        }
+        if (negative == positive) {
+          data["onclick_comments_sentiment"] = "neutral";
+          answer_sentiment.push(0.5);
+        }
+        DATA.push(data);
+      }
+      //Final JSON obtained.............
+      callback && callback(DATA);
+      // self.sentimentAnalysis(comments_count, comments_body, function (result) {
+      //   callback(result);
+      // });
+    }, 2000);
   }
 
   getStackExchangeQuestionById(questionID, callback) {
@@ -111,6 +174,24 @@ class searchQueryHelper {
         console.log(error);
       });
   }
+
+  // sentimentAnalysis(comments_count, comments_body, callback) {
+  //   callback(comments_count);
+  //   var k = 0;
+  //   var comments_sentiment = [];
+  //   console.log("Going for sentiment analysis");
+  //   for (var i = 0; i < comments_count.length; i++) {
+  //     console.log(comments_count[i]);
+  //     // if(comments_count[i] == 0){
+  //     //   comments_sentiment.append("neutral")
+  //     // }
+  //     // else{
+  //     //     for(var j=0; j<comments_count[i]; j++){
+
+  //     //     }
+  //     // }
+  //   }
+  // }
 }
 
 module.exports = searchQueryHelper;
