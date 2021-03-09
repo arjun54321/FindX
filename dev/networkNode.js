e218ad551d597eb27e65dcc4fea79e421c5c85b6
arjun.panwar <arjun.panwar@tothenew.com>
2021-03-09 13:19:34 +0530

    added

---
+++ dev/networkNode.js
@@ -0,0 +1,377 @@
+var express = require('express')
+const bodyParser = require('body-parser')
+const Blockchain = require('./blockchain')
+const rp = require('request-promise')
+const axios = require("axios");
+const { v4: uuidv4 } = require('uuid')
+const searchQueryHelper = require("./searchQueryHelper");
+const searchQuery = new searchQueryHelper();
+// Watson languageTranslator..........
+const LanguageTranslatorV3 = require("ibm-watson/language-translator/v3");
+const { IamAuthenticator } = require("ibm-watson/auth");
+// const requestPromise = require('request-promise')
+const nodeAddress = uuidv4()
+const bitcoin = new Blockchain()
+var app = express()
+app.use(express.static(__dirname + '/block-explorer/public'));
+const port = process.argv[2]
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/blockchain', function (req, res) {
+  res.send(bitcoin)
+})
+
+app.post('/transaction', function (req, res) {
+  const newTransaction = req.body
+  const blockIndex = bitcoin.addTransactionToPendingTransactions(newTransaction)
+  res.json({ note: `Transaction will be added in block ${blockIndex}.` })
+})
+
+app.post('/transaction/broadcast', function (req, res) {
+  const newTransaction = bitcoin.createNewTransaction(
+    req.body.queryResult,
+    req.body.sender,
+    req.body.recipient
+  )
+  bitcoin.addTransactionToPendingTransactions(newTransaction)
+  const requestPromises = []
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/transaction',
+      method: 'POST',
+      body: newTransaction,
+      json: true
+    }
+    requestPromises.push(rp(requestOptions))
+  })
+  Promise.all(requestPromises).then(data => {
+    res.json({ note: 'Transaction created & broadcast successfully.' })
+  })
+})
+
+app.get('/mine', function (req, res) {
+  const lastBlock = bitcoin.getLastBlock()
+  const previousBlockHash = lastBlock['hash']
+
+  var blockchain_answer_id = [];
+  var chain = bitcoin.chain;
+  for(var i=0; i<chain.length; i++) {
+    if(chain[i].transactions.length != false) {
+      var transactions = chain[i].transactions;
+      for(var a=0;a<transactions.length;a++) {
+        var query = transactions[a].queryResult;
+        for(var b=0;b<query.length;b++) {
+          blockchain_answer_id.push(query[b].answer_id);
+        }
+      }
+    }
+  }
+  var data = bitcoin.pendingTransactions;
+  for(var i=0; i<data.length; i++) {
+    if(data[i].queryResult.length != false) {
+      var transactions = data[i].queryResult;
+      for(var j=0; j<transactions.length; j++) {
+        // console.log(transactions[j].answer_id);
+        for(var k=0; k<blockchain_answer_id.length; k++) {
+          if(transactions[j].answer_id == blockchain_answer_id[k]) {
+            console.log(transactions[j].answer_id);
+            transactions.splice(j,1);
+            // break;
+          }
+        }
+      }
+
+    }
+  }
+
+  const currentBlockData = {
+    transactions: bitcoin.pendingTransactions,
+    index: lastBlock['index'] + 1
+  }
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData)
+  const blockHash = bitcoin.hashBlock(
+    previousBlockHash,
+    currentBlockData,
+    nonce
+  )
+  const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash)
+  const requestPromises = []
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/recive-new-block',
+      method: 'POST',
+      body: { newBlock: newBlock },
+      json: true
+    }
+    requestPromises.push(rp(requestOptions))
+  })
+  Promise.all(requestPromises)
+    .then(data => {
+      res.json({
+        note: 'New block mined & broadcast successfully',
+        block: newBlock
+      })
+    })
+})
+
+app.post('/recive-new-block', function (req, res) {
+  const newBlock = req.body.newBlock
+  const lastBlock = bitcoin.getLastBlock()
+  const correctHash = lastBlock.hash === newBlock.previousBlockHash
+  const correctIndex = lastBlock['index'] + 1 === newBlock['index']
+  if (correctHash && correctIndex) {
+    bitcoin.chain.push(newBlock)
+    bitcoin.pendingTransactions = []
+    res.json({
+      note: 'New block received and accepted',
+      newBlock: newBlock
+    })
+  } else {
+    res.json({
+      note: 'New block rejected',
+      newBlock: newBlock
+    })
+  }
+})
+
+app.post('/register-and-broadcast-node', function (req, res) {
+  const newNodeUrl = req.body.newNodeUrl
+  const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1
+  const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl
+  if (nodeNotAlreadyPresent && notCurrentNode) {
+    bitcoin.networkNodes.push(newNodeUrl)
+  }
+  const registerNodesPromises = []
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/register-node',
+      method: 'POST',
+      body: { newNodeUrl: newNodeUrl },
+      json: true
+    }
+    registerNodesPromises.push(rp(requestOptions))
+  })
+  Promise.all(registerNodesPromises)
+    .then(data => {
+      console.log(newNodeUrl)
+      const bulkRegisterOptions = {
+        uri: newNodeUrl + '/register-nodes-bulk',
+        method: 'POST',
+        body: {
+          allNetworkNodes: [...bitcoin.networkNodes, bitcoin.currentNodeUrl]
+        },
+        json: true
+      }
+      return rp(bulkRegisterOptions)
+    })
+    .then(data => {
+      res.json({
+        note: 'New node registered with other networks successfully!'
+      })
+    })
+})
+
+app.post('/register-node', function (req, res) {
+  const newNodeUrl = req.body.newNodeUrl
+  const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1
+  const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl
+  if (nodeNotAlreadyPresent && notCurrentNode) {
+    bitcoin.networkNodes.push(newNodeUrl)
+  }
+  res.json({
+    note: 'New node registered successfully.'
+  })
+})
+
+app.post('/register-nodes-bulk', function (req, res) {
+  const allNetworkNodes = req.body.allNetworkNodes
+  allNetworkNodes.forEach(networkNodeUrl => {
+    const nodeNotAlreadyPresent =
+      bitcoin.networkNodes.indexOf(networkNodeUrl) == -1
+    const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl
+    if (nodeNotAlreadyPresent && notCurrentNode) {
+      bitcoin.networkNodes.push(networkNodeUrl)
+    }
+  })
+  res.json({
+    note: 'Bulk registration successfull.'
+  })
+})
+
+app.get('/consensus', function (req, res) {
+  const requestPromises = []
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/blockchain',
+      method: 'GET',
+      json: true
+    }
+
+    requestPromises.push(rp(requestOptions))
+  })
+
+  Promise.all(requestPromises).then(blockchains => {
+    const currentChainLength = bitcoin.chain.length
+    let maxChainLength = currentChainLength
+    let newLongestChain = null
+    let newPendingTransactions = null
+
+    blockchains.forEach(blockchain => {
+      if (blockchain.chain.length > maxChainLength) {
+        maxChainLength = blockchain.chain.length
+        newLongestChain = blockchain.chain
+        newPendingTransactions = blockchain.pendingTransactions
+      }
+    })
+
+    if (
+      !newLongestChain ||
+      (newLongestChain && !bitcoin.chainIsValid(newLongestChain))
+    ) {
+      res.json({
+        note: 'Current chain has not been replaced.',
+        chain: bitcoin.chain
+      })
+    } else {
+      bitcoin.chain = newLongestChain
+      bitcoin.pendingTransactions = newPendingTransactions
+      res.json({
+        note: 'This chain has been replaced.',
+        chain: bitcoin.chain
+      })
+    }
+  })
+})
+
+app.get('/block/:blockHash', function (req, res) {
+  const blockHash = req.params.blockHash
+  const correctBlock = bitcoin.getBlock(blockHash)
+  res.json({
+    block: correctBlock
+  })
+})
+
+app.get('/transaction/:transactionId', function (req, res) {
+  const transactionId = req.params.transactionId
+  const transactionData = bitcoin.getTransaction(transactionId)
+  res.json({
+    transaction: transactionData.transaction,
+    block: transactionData.block
+  })
+})
+
+app.get('/address/:address', function (req, res) {
+  const address = req.params.address
+  const addressData = bitcoin.getAddressData(address)
+  res.json({
+    addressData: addressData
+  })
+})
+
+app.get('/block-explorer', function (req, res) {
+  res.sendFile('./block-explorer/index.html', { root: __dirname })
+})
+
+app.get('/home', function (req, res) {
+  res.sendFile('./block-explorer/home.html', { root: __dirname })
+})
+
+// Watson Language Translator API Configuration.....
+const languageTranslator = new LanguageTranslatorV3({
+  version: "2018-05-01",
+  authenticator: new IamAuthenticator({
+    apikey: "owATBr1PcPdXG9KsV0jEe_-xxtvrypJ7gAYHl7uyxVcm",
+  }),
+  serviceUrl:
+    "https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/487d1371-8841-4279-a79e-73336dd67bea",
+});
+
+// Get input Query value.........
+app.get("/home/:queryString/:questionCount/:questionlanguage", function (req, res) {
+  var queryString = req.params.queryString;
+  const questionCount = req.params.questionCount;
+  var questionlanguage = req.params.questionlanguage;
+  var hostname = req.headers.host;
+  var protocol = req.protocol;
+  var transactionURL = protocol + "://" + hostname + "/transaction/broadcast";
+  const identifyParams = {
+    text: queryString,
+  };
+  languageTranslator
+    .identify(identifyParams)
+    .then((identifiedLanguages) => {
+      var language = identifiedLanguages.result.languages[0].language;
+      if (language !== "en") {
+        const translateParams = {
+          text: queryString,
+          modelId: `${language}-en`,
+        };
+        languageTranslator
+          .translate(translateParams)
+          .then((translationResult) => {
+            var queryString =
+              translationResult.result.translations[0].translation;
+            searchQuery.stackExchange(
+              queryString,
+              questionCount,
+              questionlanguage,
+              function (result) {
+                axios.post(transactionURL, {
+                  queryResult: result,
+                  sender: "StackOverflow",
+                  recipient: "FindX",
+                })
+                .then(function (response) {
+                  if (response.status == 200) {
+                    res.status(200).json({
+                      status: "success",
+                      data: result,
+                    });
+                  }
+                  else {
+                    console.log("Something went wrong");
+                  }
+                })
+              }
+            );
+          })
+          .catch((err) => {
+            res.sendStatus("error:", err);
+          });
+      } else {
+        searchQuery.stackExchange(
+          queryString,
+          questionCount,
+          questionlanguage,
+          function (result) {
+            axios.post(transactionURL, {
+              queryResult: result,
+              sender: "StackOverflow",
+              recipient: "FindX",
+            })
+            .then(function (response) {
+              if (response.status == 200) {
+                res.status(200).json({
+                  status: "success",
+                  data: result,
+                });
+              }
+              else {
+                console.log("Something went wrong");
+              }
+            })
+          }
+        );
+      }
+    })
+    .catch((err) => {
+      res.sendStatus("error:", err);
+    });
+});
+
+app.listen(port, function () {
+  console.log(`listening on port ${port}...`)
+})
---
+++ dev/searchQueryHelper.js
@@ -0,0 +1,284 @@
+const axios = require("axios");
+var Sentiment = require("sentiment");
+var sentiment = new Sentiment();
+const Auth_key = "36MG*PWxNRuR2IurlPc8vg((";
+var questionLink = [];
+var questionID = [];
+var questionTitle = [];
+var actualTags = [];
+var answers_per_question = [];
+var comments_count = [];
+var comments_body = [];
+var answer_id = [];
+var answer_accepted = [];
+var score_answer = [];
+var auth_repo = [];
+var comments_sentiment = [];
+var answer_sentiment = [];
+var index = 0;
+var positive = 0;
+var negative = 0;
+var neutral = 0;
+class searchQueryHelper {
+  stackExchange(queryString, questionCount, questionlanguage, callback) {
+    var page_size = parseInt(questionCount) + 10;
+    const query = queryString.toString() + " in " + questionlanguage;
+    const tagged = questionlanguage;
+    var stackOverflowUserURL =
+      "https://api.stackexchange.com/2.2/similar?key=" +
+      Auth_key +
+      "&order=desc&page=1&pagesize=" +
+      page_size +
+      "&sort=relevance&title=" +
+      query +
+      "&tagged=" +
+      tagged +
+      "&site=stackoverflow";
+    var self = this;
+    axios
+      .get(stackOverflowUserURL)
+      .then((response) => {
+        if (response.status == 200) {
+          self.getStackExchangeData(
+            response.data,
+            questionCount,
+            function (result) {
+              callback && callback(result);
+            }
+          );
+        } else {
+          console.log("Error Occured");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  getStackExchangeData(questionData, questionCount, callback) {
+    var totalLinks = questionData["items"].length;
+
+    questionData["items"].forEach(function (item, index) {
+      if (!item["is_answered"] || item["score"] < 0) {
+        // pass
+      } else {
+        questionID.push(item["question_id"]);
+        actualTags.push(item["tags"]);
+        questionLink.push(item["link"]);
+        answers_per_question.push(item["answer_count"]);
+        questionTitle.push(item["title"]);
+      }
+    });
+
+    // Get questions by id
+    var self = this;
+    self.getStackExchangeQuestionById(function (result) {
+      self.sentimentAnalysis(function (result) {
+        var Data = [];
+        var final_data;
+        var final_data_positive = [];
+        var final_data_neutral = [];
+        var final_data_negative = [];
+        var final_data_positive_false = [];
+        var final_data_neutral_false = [];
+        var final_data_negative_false = [];
+
+        //On the basis of Author repo..................
+        var zipped = answer_id.map(function (e, i) {
+          return [e, comments_sentiment[i], answer_accepted[i]];
+        });
+        var sorted = zipped.sort(function (answer_id, auth_repo) {
+          return answer_id[1] - auth_repo[1];
+        });
+        // const arrayColumn = (arr, n) => arr.map((x) => x[n]);
+        // sorted_answer_id_auth_repo = arrayColumn(sorted, 0);
+        // sorted_answer_id_auth_repo.reverse();
+
+        sorted.forEach(function(item){
+          if(item[1] == 'positive' && item[2] == true) {
+            final_data_positive.push(item[0]);
+          }
+          if(item[1] == 'neutral' && item[2] == true) {
+            final_data_neutral.push(item[0]);
+          }
+          if(item[1] == 'negative' && item[2] == true) {
+            final_data_negative.push(item[0]);
+          }
+          if(item[1] == 'positive' && item[2] == false) {
+            final_data_positive_false.push(item[0]);
+          }
+          if(item[1] == 'neutral' && item[2] == false) {
+            final_data_neutral_false.push(item[0]);
+          }
+          if(item[1] == 'negative' && item[2] == false) {
+            final_data_negative_false.push(item[0]);
+          }
+        })
+
+        final_data = final_data_positive.concat(final_data_neutral, final_data_negative, final_data_positive_false, final_data_neutral_false, final_data_negative_false);
+        final_data = final_data.slice(0, parseInt(questionCount));
+
+        for (var k=0; k<final_data.length; k++) {
+          var data = {};
+          for(var i=0; i<result.length; i++) {
+            for(var j=0; j<result[i].answer.length; j++) {
+              if (result[i].answer[j].answer_id == final_data[k]) {
+                data['title'] = result[i].title;
+                data['link'] = result[i].link;
+                data['answer_id'] = result[i].answer[j].answer_id;
+              }
+            }
+          }
+          Data.push(data);
+        }
+
+        // Remove Duplicates from object..........
+        var jsonObject = Data.map(JSON.stringify);
+        var uniqueSet = new Set(jsonObject);
+        var uniqueArray = Array.from(uniqueSet).map(JSON.parse);
+
+        // Initialize variable length zero......
+        questionLink.length = 0;
+        questionID.length = 0;
+        questionTitle.length = 0;
+        actualTags.length = 0;
+        answers_per_question.length = 0;
+        comments_count.length = 0;
+        comments_body.length = 0;
+        answer_id.length = 0;
+        answer_accepted.length = 0;
+        score_answer.length = 0;
+        auth_repo.length = 0;
+        comments_sentiment.length = 0;
+        answer_sentiment.length = 0;
+        positive = 0;
+        negative = 0;
+
+        console.log(result);
+        callback(uniqueArray);
+      });
+    });
+  }
+
+  getStackExchangeQuestionById(callback) {
+    var stackOverflowQuestion =
+      "https://api.stackexchange.com/2.2/questions/" +
+      questionID[index] +
+      "/answers?key=" +
+      Auth_key +
+      "&order=desc&sort=votes&site=stackoverflow&filter=!LfpZw1(lSuQgJ8v-9BuJ.1";
+    axios
+      .get(stackOverflowQuestion)
+      .then((response) => {
+        if (response.status == 200) {
+          var result = response.data;
+          console.log(questionID[index] + " => answerid => ");
+          for (var i = 0; i < answers_per_question[index]; i++) {
+            if (typeof result.items[i] !== "undefined") {
+              console.log(result.items[i].answer_id);
+              comments_count.push(result.items[i].comment_count);
+              answer_id.push(result.items[i].answer_id);
+              score_answer.push(result.items[i].score);
+              answer_accepted.push(result.items[i].is_accepted);
+              auth_repo.push(result.items[i].owner.reputation);
+
+              // Get Comments Body.......
+              for (var j = 0; j < result.items[i].comment_count; j++) {
+                console.log(result.items[i].comments[j].body);
+                comments_body.push(result.items[i].comments[j].body);
+                var comment_string = result.items[i].comments[j].body;
+                var analysis = sentiment.analyze(comment_string);
+                if (analysis.comparative > 0) {
+                  // comments_sentiment.push("positive");
+                  positive += 1;
+                } else if (analysis.comparative == 0) {
+                  // comments_sentiment.push("neutral");
+                  neutral += 1;
+                } else {
+                  // comments_sentiment.push("negative");
+                  negative += 1;
+                }
+              }
+              if (positive > negative && positive >= neutral) {
+                comments_sentiment.push("positive");
+              } else if (negative > neutral && negative > positive) {
+                comments_sentiment.push("negative");
+              } else {
+                comments_sentiment.push("neutral");
+              }
+              positive = 0;
+              negative = 0;
+              neutral = 0;
+            } else {
+              console.log("Not Defined");
+            }
+          }
+        } else {
+          console.log("Error Occured");
+        }
+        index++;
+        if (questionID.length == index) {
+          index = 0;
+          callback();
+        } else {
+          // console.log("id : " + questionID[index] + ", index : " + index);
+          this.getStackExchangeQuestionById(callback);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  sentimentAnalysis(callback) {
+    //Sentimental Analysis for answers.............
+    var DATA = [];
+    var jindex = 0;
+    for (var i = 0; i < questionLink.length; i++) {
+      positive = 0;
+      neutral = 0;
+      negative = 0;
+      var data = {};
+      data["link"] = questionLink[i];
+      data["id"] = questionID[i];
+      data["title"] = questionTitle[i];
+      data["actualTags"] = actualTags[i];
+      var ANSWERS = [];
+      for (var j = 0; j < answers_per_question[i]; j++) {
+        var answer = {};
+        answer["is_accepted"] = answer_accepted[jindex];
+        answer["answer_id"] = answer_id[jindex];
+        answer["score_answer"] = score_answer[jindex];
+        answer["authRepo"] = auth_repo[jindex];
+        answer["sentiment"] = comments_sentiment[jindex];
+        if (comments_sentiment[jindex] == "positive") {
+          positive += 1;
+        }
+        if (comments_sentiment[jindex] == "negative") {
+          negative += 1;
+        }
+        if (comments_sentiment[jindex] == "neutral") {
+          neutral += 1;
+        }
+        ANSWERS.push(answer);
+        jindex++;
+      }
+      data["answer"] = ANSWERS;
+      if (positive > negative && positive >= neutral) {
+        data["onclick_comments_sentiment"] = "positive";
+        answer_sentiment.push(1);
+      } else if (negative > neutral && negative > positive) {
+        data["onclick_comments_sentiment"] = "negative";
+        answer_sentiment.push(0);
+      } else {
+        data["onclick_comments_sentiment"] = "neutral";
+        answer_sentiment.push(0.5);
+      }
+      DATA.push(data);
+    }
+    //Final JSON obtained.............
+    callback && callback(DATA);
+  }
+}
+
+module.exports = searchQueryHelper;
